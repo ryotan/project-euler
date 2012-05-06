3fe252d917cb6d22455f506148e295344fd1151a
Ryo TANAKA <ryotan@ucb-jp.net>
2012-05-06 22:24:34 +0900

    Fixed test case comment.
    not "under" but "below".

--- java/src/test/java/euler/java/Problem1Test.java
+++ java/src/test/java/euler/java/Problem1Test.java
@@ -22,8 +22,8 @@ public class Problem1Test {
      */
     @Test
     public void testFizzBussSum() throws Exception {
-        assertEquals("fizz buzz summation under 10 is 23", testee.fizzBuzzSum(10), 23L);
-        assertThat("fizz buzz summation under 13 is 35", testee.fizzBuzzSum(13), is(45L));
-        assertThat("fizz buzz summation under 1000 is ??", testee.fizzBuzzSum(1000), is(233168L));
+        assertEquals("fizz buzz summation below 10 is 23", testee.fizzBuzzSum(10), 23L);
+        assertThat("fizz buzz summation below 13 is 35", testee.fizzBuzzSum(13), is(45L));
+        assertThat("fizz buzz summation below 1000 is ??", testee.fizzBuzzSum(1000), is(233168L));
     }
 }
